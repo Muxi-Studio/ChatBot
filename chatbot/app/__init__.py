1d2231e645376c6a5a1fa634b4d74d967dc8a310
kasheemlew <417879751@qq.com>
2016-07-13 18:10:38 +0800

    revise pymongo version error

--- chatbot/app/__init__.py
+++ chatbot/app/__init__.py
@@ -4,12 +4,17 @@ import os
 import pymongo
 from flask import Flask
 
+try:
+    from pymongo.connection import Connection
+except ImportError as e:
+    from pymongo import MongoClient as Connection
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret key'
 
 host = os.environ.get('MONGODB_URL')
-conn = pymongo.Connection(host=host,port=27017)
+conn = Connection(host=host,port=27017)
 db = conn.chatbot
 
 from . import views, forms
