202329608d66fb84648452b5eb4d3756c185474a
roseou <rose@roseoudeMacBook-Pro.local>
2016-08-03 15:57:20 +0800

    chatbot

--- chatbot/app/views.py
+++ chatbot/app/views.py
@@ -8,8 +8,8 @@ import jieba.posseg as pseg
 jieba.load_userdict("dic.txt")
 
 
-@app.route('/<question>', methods=['GET','POST'])
-def index(question):
+@app.route('/<text>', methods=['GET','POST'])
+def index(text):
     tag = 'unk'
     keyword = None
     content = None
@@ -19,8 +19,8 @@ def index(question):
     weblist = [u'网址', u'网页', u'网站']
     txtlist = [u'通知', u'资料', u'公告']
     piclist = [u'照片', u'相片', u'图片']
-    keywords = jieba.analyse.extract_tags(question,10)
-    psegword = pseg.cut(question)
+    keywords = jieba.analyse.extract_tags(text,10)
+    psegword = pseg.cut(text)
     for n in psegword:
         keydict[n.word] = n.flag
     for i in keywords:
