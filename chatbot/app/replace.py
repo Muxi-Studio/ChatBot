c26bf61690d8881ee618ddca38cede23796efcaf
roseou <598959770@qq.com>
2016-08-23 20:15:26 +0800

    commit

---
+++ chatbot/app/replace.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+def replace_word(text):
+	words = {'心理学院':'华中师范大学田家炳楼',
+			'9号楼':'华中师范大学9号教学楼',
+			'9号教学楼':'华中师范大学9号教学楼',
+			'8号楼':'华中师范大学8号教学楼',
+			'8号教学楼':'华中师范大学8号教学楼'
+	}
+	for i in words:
+		text = text.replace(i,words[i])
+	return text
